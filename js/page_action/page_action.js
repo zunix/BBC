a28cc30ab18d44d8ca8156d35808b457f24f7e3e
Thomas Wolf <thomas.wolf@BA-W-141.office.brand-ad.de>
2016-01-14 16:27:27 +0100

    Popup Page
    HTML Edits,
    added EventListener

--- js/page_action/page_action.js
+++ js/page_action/page_action.js
@@ -1,3 +1,10 @@
 $( document ).ready(function() {
     console.log( "ready!" );
+    document.getElementById("myBtn").addEventListener("click", function(e){
+        var colorVars= $('input[type=color]').map(function(){
+            return "@"+this.name+"\" : \""+this.value+"";//this.value;
+        }).get();
+        console.log( colorVars );
+
+    });
 });
