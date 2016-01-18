058b2df7d73eca751d0abaec5909194e42047ce3
zunix <zunix.pdc@gmail.com>
2016-01-18 15:58:30 +0100

    console message clean up

---
+++ src/inject/utils/consoleMessages.js
@@ -0,0 +1,11 @@
+function showErrorMessage(errorMessage) {
+	console.log('%c [BBC Error] '+errorMessage+' ', 'background: #FFEFEF; color: red');
+}
+
+function showSuccessMessage(successMessage) {
+	console.log('%c [BBC Success] '+successMessage+' ', 'background: #ebffcc; color: #639a0d');
+}
+
+function showInfoMessage(infoMessage) {
+	console.log('%c [BBC Information] '+infoMessage+' ', 'background: #ccf6ff; color: #0d819a');
+}
