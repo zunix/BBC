0160f34d87d33954f826a7c1fd7bbf0cd1b56b0e
zunix <zunix.pdc@gmail.com>
2016-01-19 11:47:43 +0100

    bbc_ExtensionEventHandler done

--- src/inject/bbc_ExtensionEventHandler.js
+++ src/inject/bbc_ExtensionEventHandler.js
@@ -1,56 +1,11 @@
-// console.log("whoop whoop");
-// showErrorMessage("awwwwwwwwwwwww");
-//
-//
-// sendMessage();
-//
-//
-// function sendMessage() {
-//   chrome.runtime.sendMessage(
-//       "foo",
-//       function (response) {
-//           if(response === 'undefined'){
-//               console.log(response);
-//               sendMessage();
-//           }
-//       }
-//   );
-//   console.log("mohmoh");
-// }
-// var bbc_tag = document.getElementsByTagName("bbc")[0];
-//
-//
-//
-// var bbc_ext_event = new Event('bbc_ext_event');
-//
-//
-// // Dispatch the event.
-// bbc_tag.dispatchEvent(bbc_ext_event);
-
-function magicalTree(hexColor) {
-    location.href="javascript:greeting('"+hexColor+"'); void 0";
-    //do magic here!
-}
-
-function dispatchChangeEvent(data) {
-  location.href="javascript:updatePage('"+data+"'); void 0";
-}
-
 chrome.runtime.onMessage.addListener(
-  function (request,sender) {
-      console.log(request);
-      console.log(sender);
-      if(request.pewpew == 'poi poi'){
-        var hexColor = '#'+Math.floor(Math.random()*16777215).toString(16);
-        magicalTree(hexColor);
-      }
-
-      if(request.changeEvent){
-        dispatchChangeEvent(request.changeEvent);
-      }
+  function(request, sender) {
+    if (request.changeEvent) {
+      dispatchChangeEvent(request.changeEvent);
+    }
   }
 );
 
-var evt = document.createEvent('Event');
-evt.initEvent('myCustomEvent', true, false);
-document.dispatchEvent(evt);
+function dispatchChangeEvent(data) {
+  location.href = "javascript:updatePage('" + data + "'); void 0";
+}
